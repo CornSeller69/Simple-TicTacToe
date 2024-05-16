74c3a0f16282e9561f00662f85e09e8e1f87c604
CornSeller <166633175+CornSeller69@users.noreply.github.com>
2024-05-16 14:21:32 +0200

    Updated style, script and main html doc
    Some new stuff 'n' all :trollface:

--- files/skrypt1.js
+++ files/skrypt1.js
@@ -1,21 +1,22 @@
-let parag = document.getElementById("p1");
+let barag = document.getElementById("p1");
 let gameStarted = false;
 
-let starter = function() {
-    if(gameStarted) {alert("Gra obecnie już trwa!");} else if(!gameStarted) {
+let starter = function () {
+    if (gameStarted) { alert("Gra obecnie już trwa!"); } else if (!gameStarted) {
         gameStarted = true;
         checkNames();
     }
 }
 
-let checkNames = function() {
+let checkNames = function () {
     let a = prompt("Podaj nazwę Pierwszego gracza");
     let b = prompt("Podaj nazwę Drugiego gracza");
-    if (!a || !b) {checkNames();} else if(a && b) {startGame();}
+    if (!a || !b) { checkNames(); } else if (a && b) { startGame(); }
     console.log("Player1: " + a + ", Player2: " + b);
-    parag.textContent = "Gracz 1: " + a + " | " + "Gracz 2: " + b;
+    barag.textContent = "Gracz 1: " + a + " | " + "Gracz 2: " + b;
 }
 
-let startGame = function() {
-    console.log("placeholder text: działa startGame() btw   :)")
+let startGame = function () {
+    console.log("startGame(): Jak narazie jest wszystko git!");
+    
 }
